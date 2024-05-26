3870cba9eddb9d6a5162254be6f0eb417f92e637
jom-hedges <jom_hedges@proton.me>
2024-05-26 19:11:14 +0800

    added integrations: turso, react, sitemap, and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [mdx(), sitemap(), react()]
+});
