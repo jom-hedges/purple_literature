333d051a37916f69decb8764d39361365acf607c
jom-hedges <jom_hedges@proton.me>
2024-05-28 14:54:44 +0800

    getting some eyecandy for index.astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Any global data goes in this file.
+// Import this data anywhere in the site.
+
+export const SITE_TITLE = 'Osmanthus';
+export const SITE_DESCRIPTION = 'A magazine of culture, technology, and poetry';
